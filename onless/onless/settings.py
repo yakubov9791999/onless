e8df7df31ac787f73eeba8b4e2b208953c0d6fe3
yakubov <yakubov9791999@gmail.com>
2020-05-31 11:13:11 +0500

    configuring settings

--- onless/onless/settings.py
+++ onless/onless/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'onless.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]

968fd89e7aa5c2fad068da24026c9ca9e76e99ec
yakubov <yakubov9791999@gmail.com>
2020-05-31 16:02:28 +0500

    fix

--- onless/onless/urls.py
+++ onless/onless/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('quiz.urls')),
 ]
 
 if settings.DEBUG:

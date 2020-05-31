dc8044d0f6eb2928f912378a25ed76009c0c4b0a
yakubov <yakubov9791999@gmail.com>
2020-05-31 11:06:11 +0500

    add requiremets.txt

--- onless/user/admin.py
+++ onless/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'password', 'is_active', 'is_superuser', 'is_staff', 'date_joined', 'last_login']
+    list_display_links = ['username', 'email']

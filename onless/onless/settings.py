98706b63b3db3cdf04312e1c69e0764b2689ec12
yakubov <yakubov9791999@gmail.com>
2020-05-31 11:04:44 +0500

    add custom user

--- onless/onless/settings.py
+++ onless/onless/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'quiz.apps.QuizConfig',
+    'video.apps.VideoConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
+AUTH_USER_MODEL = 'user.User'
--- onless/user/models.py
+++ onless/user/models.py
@@ -1,3 +1,73 @@
+import os
+from uuid import uuid4
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+def path_and_rename(instance, filename):
+    upload_to = 'user_avatars/'
+    ext = filename.split('.')[-1]
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    return os.path.join(upload_to, filename)
+
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email,username, password, is_staff, is_superuser, **extra_fields):
+        if not email:
+            raise ValueError('Emailni kiritish majburiy')
+        if not username:
+            raise ValueError('Usernameni kiritish majburiy')
+        now = timezone.now()
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, region, password, **extra_fields):
+        return self._create_user(email, username, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        user = self._create_user(email, username, password, True, True, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    birthday = models.DateTimeField(max_length=120, blank=True, null=True)
+    username = models.CharField(max_length=30, unique=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False, blank=True)
+    is_active = models.BooleanField(default=True, blank=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def get_absolute_url(self):
+        return "/users/%i/" % (self.pk)
+
+    def __str__(self):
+        return f"{self.username}"

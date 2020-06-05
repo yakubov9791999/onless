a2379eab4d79ba176967133bc2ff89327d30d605
yakubov <yakubov9791999@gmail.com>
2020-06-05 15:41:22 +0500

    add logo

--- onless/user/models.py
+++ onless/user/models.py
@@ -67,6 +67,7 @@ class DrivingSchool(models.Model):
     title = models.CharField(max_length=255)
     director_fio = models.CharField(max_length=255, blank=True)
     phone = models.CharField(max_length=20, blank=True)
+    logo = models.ImageField(upload_to='school/')
 
     def __str__(self):
         return self.title

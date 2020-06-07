9ec28832ae1d61fc70b7d499c3d1082c605ab09c
yakubov <yakubov9791999@gmail.com>
2020-06-07 22:29:58 +0500

    set

--- onless/onless/settings/prod.py
+++ onless/onless/settings/prod.py
@@ -1,5 +1,20 @@
 from .base import *
 
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'bcloudintelekt_onless',
+        'USER': 'bcloudintelekt',
+        'PASSWORD': 'm6232971',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
 try:
     from .local import *
 except:

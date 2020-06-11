2ed148be18cdb130066472e2017e4f99f38a8aed
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2020-06-11 21:26:12 +0500

    settings

--- onless/onless/settings.py
+++ onless/onless/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'onless.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'bcloudintelekt_onless',
-        'USER': 'bcloudintelekt',
-        'PASSWORD': 'm6232971',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': 'bcloudintelekt_onless',
+        # 'USER': 'bcloudintelekt',
+        # 'PASSWORD': 'm6232971',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 

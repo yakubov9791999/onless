77570ae56e872074124059586fea25f1d8174b90
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2020-06-15 18:10:04 +0500

    settings

--- onless/onless/settings.py
+++ onless/onless/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(kbqdmc#xti0zu85h=u8!^qcoqf)u_yg67oj1bdi2_1f6=qlxe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

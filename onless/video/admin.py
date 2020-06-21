e0522822c2c6706bd19558c97f136a55696c997a
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2020-06-21 12:16:55 +0500

    fixed admin  sigunschool

--- onless/video/admin.py
+++ onless/video/admin.py
@@ -29,7 +29,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(SignUpSchool)
 class SignUpSchoolAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'select', 'phone', 'region', 'district', 'text']
+    list_display = ['id', 'name', 'select','is_active', 'phone', 'region', 'district', 'text']
     list_display_links = ['name']
     list_filter = ['select','region', 'district']
     save_on_top = True

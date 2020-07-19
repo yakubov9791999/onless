3a341031f1819f50ac6af16c647addffba8e7222
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2020-07-19 18:27:53 +0500

    debug True

--- onless/onless/urls.py
+++ onless/onless/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('quiz/', include('quiz.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

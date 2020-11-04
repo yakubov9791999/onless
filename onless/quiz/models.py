8e1b1620291004b8f2119b23b06952daf245f3ef
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2020-11-04 09:55:05 +0500

    uncomment

--- onless/quiz/models.py
+++ onless/quiz/models.py
@@ -76,17 +76,6 @@ class Javob(models.Model):
         verbose_name = 'Test javobi'
         verbose_name_plural = 'Test javoblari'
 
-# class Result(models.Model):
-#     question = models.ForeignKey(Savol, on_delete=models.CASCADE, related_name='result_savol', null=True)
-#     answer = models.ForeignKey(Javob, on_delete=models.SET_NULL, related_name='result_javob', null=True)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='result_user', blank=True, null=True)
-#
-#     def __str__(self):
-#         return f"{self.user}"
-#
-#     class Meta:
-#         verbose_name = 'Test natijasi'
-#         verbose_name_plural = 'Test natijalari'
 
 class ResultQuiz(models.Model):
     question = models.ForeignKey(Savol, on_delete=models.CASCADE, related_name='result_question', null=True)
@@ -100,17 +89,17 @@ class ResultQuiz(models.Model):
         verbose_name = 'Test natijasi'
         verbose_name_plural = 'Test natijalari'
 
-# class Attempt(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attempt_user', null=True)
-#     allowed = fields.IntegerRangeField(verbose_name='Ruxsat berilgan urinishlar',default=2, max_value=3)
-#     solved = fields.IntegerRangeField(verbose_name='Bajarilgan urinishlar',default=0)
-#
-#     def __str__(self):
-#         return f"{self.user}: {self.allowed}"
-#
-#     class Meta:
-#         verbose_name = 'Urinish'
-#         verbose_name_plural = 'Urinishlar'
+class Attempt(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attempt_user', null=True)
+    allowed = fields.IntegerRangeField(verbose_name='Ruxsat berilgan urinishlar',default=2, max_value=3)
+    solved = fields.IntegerRangeField(verbose_name='Bajarilgan urinishlar',default=0)
+
+    def __str__(self):
+        return f"{self.user}: {self.allowed}"
+
+    class Meta:
+        verbose_name = 'Urinish'
+        verbose_name_plural = 'Urinishlar'
 
 class ResultJavob(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='resultjavob_user', null=True)

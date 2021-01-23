b620f92b290594cc0e4f3474633297f1a42176f0
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2021-01-23 16:59:19 +0500

    fixed sms errors

--- onless/user/decorators.py
+++ onless/user/decorators.py
@@ -1,4 +1,3 @@
-
 def makename(name):
     def wrapped(self):
         names = self.split(' ')
@@ -18,12 +17,14 @@ def makename(name):
                                                                                                           'r').replace(
                 'с', 's').replace('т', 't').replace('у', 'u').replace('ш', 'sh').replace('щ', 'sh').replace('ф',
                                                                                                             'f').replace(
-                'э', 'e').replace('ы', 'i').replace('я', 'ya').replace('ў',"o'").replace('ь', "'").replace('ъ', "'").replace('*',
-                                                                                                           '').replace(
+                'э', 'e').replace('ы', 'i').replace('я', 'ya').replace('ў', "o'").replace('ь', "'").replace('ъ',
+                                                                                                            "'").replace(
+                '*',
+                '').replace(
                 '!', '').replace('~', '').replace('@', '').replace('#', '').replace('№', '').replace('$', '').replace(
                 '%', '').replace('^', '').replace(':', '').replace('&', '').replace('?', '').replace('(', '').replace(
                 ')', '').replace('-', '').replace('+', '').replace('=', '').replace('/', '').replace('<', '').replace(
-                '>', '').replace('|', '').replace('€', '').replace('`',"'")
+                '>', '').replace('|', '').replace('€', '').replace('`', "'")
 
             i += 1
             if i != 4:
@@ -41,34 +42,36 @@ def makepasport(pasport):
         pasport = str(self)
         pasport = pasport.replace('А', 'A').replace('В', 'B').replace('С', 'C').replace('Т', 'T').replace('О',
                                                                                                           'O').replace(
-            'М', 'M').replace('Р', 'P').replace(' ','')
+            'М', 'M').replace('Р', 'P').replace(' ', '')
         print(pasport)
         return pasport
 
     return wrapped
 
 
-
 def makesms(sms):
     def wrapped(self):
         sms = str(self)
         sms = sms.lower().replace('ц', 'ts').replace('ч', 'ch').replace('ю',
-                                                                              'yu').replace(
-                'а', 'a').replace('б', 'b').replace('қ', 'q').replace('қ', "o'").replace('ғ', "g'").replace('ҳ',
-                                                                                                            "h'").replace(
-                'в', 'v').replace('г', 'g').replace('д', 'd').replace('е',
-                                                                      'e').replace(
-                'ё', 'yo').replace('ж', 'j').replace('з', 'z').replace('и', 'i').replace('й', 'y').replace('к',
-                                                                                                           'k').replace(
-                'л', 'l').replace('м', 'm').replace('н', 'n').replace('о', 'o').replace('п', 'p').replace('р',
-                                                                                                          'r').replace(
-                'с', 's').replace('т', 't').replace('у', 'u').replace('ш', 'sh').replace('щ', 'sh').replace('ф',
-                                                                                                            'f').replace(
-                'э', 'e').replace('ы', 'i').replace('я', 'ya').replace('ў',"o'").replace('ь', "'").replace('ъ', "'")
+                                                                        'yu').replace(
+            'а', 'a').replace('б', 'b').replace('қ', 'q').replace('қ', "o'").replace('ғ', "g'").replace('ҳ',
+                                                                                                        "h'").replace(
+            'х',
+            "h'").replace(
+            'в', 'v').replace('г', 'g').replace('д', 'd').replace('е',
+                                                                  'e').replace(
+            'ё', 'yo').replace('ж', 'j').replace('з', 'z').replace('и', 'i').replace('й', 'y').replace('к',
+                                                                                                       'k').replace(
+            'л', 'l').replace('м', 'm').replace('н', 'n').replace('о', 'o').replace('п', 'p').replace('р',
+                                                                                                      'r').replace(
+            'с', 's').replace('т', 't').replace('у', 'u').replace('ш', 'sh').replace('щ', 'sh').replace('ф',
+                                                                                                        'f').replace(
+            'э', 'e').replace('ы', 'i').replace('я', 'ya').replace('ў', "o'").replace('ь', "'").replace('ъ', "'")
 
         return sms.capitalize()
 
     return wrapped
+
 
 @makename
 def get_name(name):
@@ -83,10 +86,3 @@ def get_pasport(pasport):
 @makesms
 def get_sms(sms):
     return sms
-
-
-
-
-
-
-

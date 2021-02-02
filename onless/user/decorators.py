8d92a484a9a080d59c8cf51ead185c68c3c83f82
yakubov9791999 <63387889+yakubov9791999@users.noreply.github.com>
2021-02-02 15:01:27 +0500

    fixed sms errors

--- onless/user/decorators.py
+++ onless/user/decorators.py
@@ -52,12 +52,13 @@ def makepasport(pasport):
 def makesms(sms):
     def wrapped(self):
         sms = str(self)
-        sms = sms.lower().replace('ц', 'ts').replace('ч', 'ch').replace('ю',
+        #filter lower
+        sms = sms.replace('ц', 'ts').replace('ч', 'ch').replace('ю',
                                                                         'yu').replace(
-            'а', 'a').replace('б', 'b').replace('қ', 'q').replace('қ', "o'").replace('ғ', "g'").replace('ҳ',
-                                                                                                        "h'").replace(
+            'а', 'a').replace('б', 'b').replace('қ', "q").replace('ў', "o'").replace('ғ', "g'").replace('ҳ',
+                                                                                                        "h").replace(
             'х',
-            "x'").replace(
+            "x").replace(
             'в', 'v').replace('г', 'g').replace('д', 'd').replace('е',
                                                                   'e').replace(
             'ё', 'yo').replace('ж', 'j').replace('з', 'z').replace('и', 'i').replace('й', 'y').replace('к',
@@ -67,8 +68,20 @@ def makesms(sms):
             'с', 's').replace('т', 't').replace('у', 'u').replace('ш', 'sh').replace('щ', 'sh').replace('ф',
                                                                                                         'f').replace(
             'э', 'e').replace('ы', 'i').replace('я', 'ya').replace('ў', "o'").replace('ь', "'").replace('ъ', "'")
+        # filter upper
+        sms = sms.replace('Ц', 'Ts').replace('Ч', 'Ch').replace('Ю','Yu').replace(
+            'А', 'A').replace('Б', 'B').replace('Қ', "Q").replace('Ғ', "G'").replace('Ҳ',"H").replace('Х',"X").replace(
+            'В', 'V').replace('Г', 'G').replace('Д', 'D').replace('Е',
+                                                                  'E').replace(
+            'Ё', 'Yo').replace('Ж', 'J').replace('З', 'Z').replace('И', 'I').replace('Й', 'Y').replace('К',
+                                                                                                       'K').replace(
+            'Л', 'L').replace('М', 'M').replace('Н', 'N').replace('О', 'O').replace('П', 'P').replace('Р',
+                                                                                                      'R').replace(
+            'С', 'S').replace('Т', 'T').replace('У', 'U').replace('Ш', 'Sh').replace('Щ', 'Sh').replace('Ф',
+                                                                                                        'F').replace(
+            'Э', 'E').replace('Я', 'Ya')
 
-        return sms.capitalize()
+        return sms
 
     return wrapped
 

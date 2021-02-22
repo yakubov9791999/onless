8a3174f61b23322471157879d10c53147304e1cb
sierus <bcloudintelekt@gmail.com>
2021-02-22 08:42:59 +0500

    uncomment

--- onless/sign/admin.py
+++ onless/sign/admin.py
@@ -26,22 +26,22 @@ class SubjectAdmin(admin.ModelAdmin):
     def get_categories(self, obj):
         return ",\n".join([p.title for p in obj.categories.all()])
 
-# @admin.register(Theme)
-# class ThemeAdmin(admin.ModelAdmin):
-#     list_display = ('title','subject', 'sort')
-#     list_filter = ['sort', ]
-#
-#     search_fields = ['title', ]
-#     save_on_top = True
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ('title','subject', 'sort')
+    list_filter = ['sort', ]
+
+    search_fields = ['title', ]
+    save_on_top = True
 
     # def get_subjects(self, obj):
     #     return ",\n".join([p.short_title for p in obj.subject.all()])
 
-# @admin.register(Schedule)
-# class ScheduleAdmin(admin.ModelAdmin):
-#     list_display = ('theme', 'subject', 'author','sort', 'created_date')
-#     list_filter = ['subject','created_date']
-#     save_on_top = True
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('theme', 'subject', 'author','sort', 'created_date')
+    list_filter = ['subject','created_date']
+    save_on_top = True
 
 
 @admin.register(Material)

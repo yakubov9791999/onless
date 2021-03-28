ebb49d65cab09a53dd68266ab77f9def3306d086
sierus <bcloudintelekt@gmail.com>
2021-03-28 15:27:17 +0500

    fixed doc bugs

--- onless/user/views.py
+++ onless/user/views.py
@@ -2067,7 +2067,7 @@ def personal_exam_doc_generate(request, id):
 
 
     doc.render(context)
-    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    response = HttpResponse()
     filename = f"{user.name} - imtihon varaqa.docx"
     content = "attachment; filename=%s" % (filename)
     response['Content-Disposition'] = content
